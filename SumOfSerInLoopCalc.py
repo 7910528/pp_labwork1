1be2de791f2f0c4bd6df39f0ee687ea139943ee5
7910528 <7910528@stud.nau.edu.ua>
2024-09-02 17:32:59 +0300

    Removing "math" library from SumOfSerInLoopCalc.py and adding function to calculate factorial of a number.

--- SumOfSerInLoopCalc.py
+++ SumOfSerInLoopCalc.py
@@ -1,7 +1,11 @@
-import math
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 sum = 0
 for n in range(1, 6):
-    term = (3**n * math.factorial(n)) / (n**n)
+    term = (3**n * factorial(n)) / (n**n)
     sum += term
     print(f"Sum after iteration {n}: {sum}")
